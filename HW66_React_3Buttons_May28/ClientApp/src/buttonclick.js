ed974c703ca889bee31c7bef709a37601fdb5a89
Henny Yoffe <hennyy1149@gmail.com>
2019-05-28 22:58:15 -0400

    May 28

---
+++ HW66_React_3Buttons_May28/ClientApp/src/buttonclick.js
@@ -0,0 +1,47 @@
+﻿import React from 'react';
+
+class ButtonClick extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            num: 0,
+            add: 1,
+            color: 'red'
+        };
+
+    }
+    onButtonClick1 = () => {
+        this.setState({ num: this.state.num + this.state.add });
+    }
+    onButtonClick2 = () =>{
+        this.setState({ add: this.state.add + 1 });
+    }
+    onButtonClick3 = () => {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        this.setState({ color: color });
+    }
+         
+
+    render() {
+      
+    return (
+        <div className="well">
+            <h4 style={{ color: this.state.color }}>{this.state.num} </h4>
+            <button onClick={this.onButtonClick1} className="btn btn-warning ">Add</button>
+            <button onClick={this.onButtonClick2} className="btn btn-warning ">{this.state.add}</button>
+            <button onClick={this.onButtonClick3} className="btn btn-warning ">Color</button>
+
+        </div>
+    );
+}
+
+}
+export default ButtonClick;
+
+
+
--- HW66_React_3Buttons_May28/ClientApp/src/index.js
+++ HW66_React_3Buttons_May28/ClientApp/src/index.js
@@ -1,19 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import ButtonClick from "./buttonclick";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+render(<ButtonClick />, document.getElementById('root'));
 
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
 
-registerServiceWorker();
+
+
+
+
+
+
+
+//Building on what we did in class, create an application that has three buttons.
+//On top of the first button, there should be a number.When the button is clicked, the number should increment.
+//On top of the second button, have a number as well (should start out as 1).
+//This number will be the "increment amount" number, so that when button 2 is pressed, the number on top of it will increment,
+//    and this will now be the new amount of how much the first button should increment its number by.
+
+//    E.g.if the user pressed button one, three times, the number on top of it will say "3".
+//If the user then presses button two one time, the number on top of it will become "2".
+//    Then, if the user presses button one again, instead of its number going up to four, it will go up to five,
+//        and then seven etc because the "increment amount" is now set to two.
+
+//        Finally, the third button should change the color of the first number to be a random color every time it is pressed.
+
+//Good luck!
